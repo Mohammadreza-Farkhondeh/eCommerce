4f0e37c8a6fd1a7ae40c2830f6f709d16eec1747
Mohammadreza-Farkhondeh <mohammr.farkhonde12@sharif.edu>
2023-07-06 22:56:14 +0330

    Create: Cart model and receivers

--- ecommerce/carts/models.py
+++ ecommerce/carts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.conf import settings
+from products.models import Product
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
-# Create your models here.
+
+class Cart(models.Model):
+    """
+    Cart model
+    oneToOne relationship with user
+    """
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class CartItem(models.Model):
+    """
+    CartItem model
+    FK with Product
+    FK with Cart
+    """
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    subtotal = models.DecimalField(max_digits=10, decimal_places=2)
--- ecommerce/users/serializers.py
+++ ecommerce/users/serializers.py
@@ -1,21 +1,18 @@
-from rest_framework.serializers import ModelSerializer, as_serializer_error
-from django.contrib.auth.password_validation import validate_password
+from rest_framework.serializers import ModelSerializer
 from django.contrib.auth import get_user_model
-from django.core.exceptions import ValidationError
-User = get_user_model()
 
 
 class UserCreateSerializer(ModelSerializer):
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ['email', ]
 
     def create(self, validated_data):
-        user = User.objects.create_user(validated_data['email'])
+        user = get_user_model().objects.create_user(validated_data['email'])
         return user
 
 
 class UserSerializer(ModelSerializer):
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ['email', 'is_active', 'is_staff']
