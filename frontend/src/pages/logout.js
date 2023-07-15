c998fa4be05f96ab59ed173679c9857f6745a49e
Mohammadreza-Farkhondeh <mohammr.farkhonde12@sharif.edu>
2023-07-15 21:46:58 +0330

    Create: Logout page

---
+++ frontend/src/pages/logout.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+import Cookies from "js-cookie";
+
+function LogoutButton() {
+    // Use history hook to navigate to other pages
+    const history = useHistory();
+
+    // Handle the logout button click
+    const handleLogout = async () => {
+    try {
+        // Get the refresh token from the cookies
+        const refreshToken = Cookies.get("refresh");
+        // Send a post request to the logout endpoint with the refresh token
+
+        // TODO: implement logout view in backend
+        //await axios.post("http://127.0.0.1:8000/api/user/logout/", { refresh });
+
+
+        // If successful, remove the tokens from the cookies and clear the authorization header
+        Cookies.remove("access");
+        Cookies.remove("refresh");
+        delete axios.defaults.headers.common["Authorization"];
+        // Redirect to the login page
+        history.push("/login");
+    } catch (err) {
+        // If error, display an alert message
+        alert(err.response.data.message);
+    }
+    };
+
+    return (
+        <div className="logout-button">
+            <button onClick={handleLogout}>Logout</button>
+        </div>
+    );
+    }
+
+export default LogoutButton;
