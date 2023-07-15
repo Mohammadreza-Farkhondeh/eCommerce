c9c5eb25600f0e87b8fc87b49173be8141a347c3
Mohammadreza-Farkhondeh <mohammr.farkhonde12@sharif.edu>
2023-07-15 21:43:47 +0330

    Create: Signup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignupForm from "./pages/signup"
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <div className="app">
+        <h1>My App</h1>
+        <Routes>
+          <Route path="/signup" element={<SignupForm />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/pages/signup.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate, useLocation } from "react-router-dom";
+
+function SignupForm() {
+  // Use state hooks to store the form data and the error message
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState(null);
+
+  // Use navigate and location hooks to handle navigation and state passing
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // Send a post request to the signup endpoint with the email
+      const response = await axios.post("http://127.0.0.1:8000/api/user/", { email });
+      // If successful, display a success message and redirect to the login page with the email as a state
+      alert(response.data.message);
+      navigate("/login", { state: { email } });
+    } catch (err) {
+      setError(err.response.data.message);
+    }
+  };
+
+  return (
+    <div className="signup-form">
+      <h1>Signup</h1>
+      {error && <p className="error">{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <button type="submit">Signup</button>
+      </form>
+    </div>
+  );
+}
+
+export default SignupForm;
